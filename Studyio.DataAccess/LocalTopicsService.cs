627488378dd7202572ac4d9622fc7e67e4c5dbed
MoaidHathot <moaid.hathot@outlook.com>
2016-01-09 00:16:40 +0200

    Adding a topics service

---
+++ Studyio.DataAccess/LocalTopicsService.cs
@@ -0,0 +1,33 @@
+﻿using Studyio.Shared.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Studyio.Shared;
+
+namespace Studyio.DataAccess
+{
+    public class LocalTopicsService : ITopicsService
+    {
+        public Task<Topic> AddTopicAsync(Topic topic)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Topic>> GetALLTopicsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Topic> RemoveTopicAsync(Topic topic)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Topic> UpdateTopicAsync(Topic topic)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
